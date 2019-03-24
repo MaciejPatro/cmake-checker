d3cb8ef19d51fe2c760f7b659d4d14ed58f7d354
Maciej Patro <maciejpatro@gmail.com>
2019-03-24 17:13:23 +0100

    Refactoring of reporters

--- cmake_checker/reporter.py
+++ cmake_checker/reporter.py
@@ -2,6 +2,20 @@ from pathlib import Path
 from junit_xml import TestSuite, TestCase
 
 
+class LineReader(object):
+    def __init__(self, file):
+        self.current_line = ''
+        self.current_line_number = 0
+        self.file = file
+
+    def get(self, line):
+        while self.current_line_number != line:
+            self.current_line_number = self.current_line_number + 1
+            self.current_line = self.file.readline()
+
+        return self.current_line
+
+
 class Reporter(object):
     @staticmethod
     def create(reporter_type: str, files_with_info: list):
@@ -16,18 +30,14 @@ class ConsoleReporter(Reporter):
     def __init__(self, files_with_info: list):
         self.files_number = len(files_with_info)
         self.files_with_info = files_with_info
-
-        self.current_violation_line = ""
-        self.current_parsing_line = 0
         self.violations = 0
 
     def generate_report(self) -> str:
-        output = ""
+        output = ''
         for file, violations in self.files_with_info:
-            output = output + self.__generate_file_violations(file, violations)
+            output += self.__generate_file_output(file, violations)
 
-        output = output + '\n' + self.__create_summary()
-        return output
+        return output + '\n' + self.__create_summary()
 
     def __create_summary(self) -> str:
         if self.violations == 0:
@@ -35,46 +45,46 @@ class ConsoleReporter(Reporter):
         else:
             return "Scanned %d file(s). Found %d issues.\n" % (self.files_number, self.violations)
 
-    def __generate_file_violations(self, file: Path, violations: list) -> str:
-        output = ""
-        with file.open() as cmake_file:
-            self.current_violation_line = ""
-            self.current_parsing_line = 0
+    def __generate_file_output(self, file: Path, violations: list) -> str:
+        output = ''
+        if violations:
+            with file.open() as cmake_file:
+                line_reader = LineReader(cmake_file)
+                output += "\n>>>" + str(file) + '<<<\n'
+                output += self.__generate_file_violations(line_reader, violations)
 
-            if violations:
-                output = output + "\n>>>" + str(file) + '<<<\n'
-
-            for (violation_type, line_number) in violations:
-                self.violations = self.violations + 1
-                self.__read_line_with_given_number(cmake_file, line_number)
-                output = output + self.__generate_new_violation(line_number, violation_type)
         return output
 
-    def __read_line_with_given_number(self, file, line):
-        while self.current_parsing_line != line:
-            self.current_parsing_line = self.current_parsing_line + 1
-            self.current_violation_line = file.readline()
+    def __generate_file_violations(self, line_reader: LineReader, violations: list):
+        output = ''
+        for (violation_type, line_number) in violations:
+            self.violations = self.violations + 1
+            output += self.__generate_new_violation_prefix(line_number, violation_type)
+            output += line_reader.get(line_number)
+        return output
 
-    def __generate_new_violation(self, line_number: int, violation_type: str):
-        return "line: %4s %20s> %s" % (line_number, violation_type, self.current_violation_line)
+    @staticmethod
+    def __generate_new_violation_prefix(line_number: int, violation_type: str):
+        return "line: %4s %20s> " % (line_number, violation_type)
 
 
 class JUnitReporter(Reporter):
     def __init__(self, files_with_info: list):
         self.files_with_info = files_with_info
-        self.current_parsing_line = 0
-        self.current_violation_line = ''
 
     def generate_report(self) -> str:
-        test_suites = []
-
-        for file_with_info in self.files_with_info:
-            test_suites.append(self.__create_test_suite_for(file_with_info))
+        test_suites = self.__generate_test_suites()
 
         if not test_suites:
             test_suites = self.__generate_empty_test_suites()
 
         return TestSuite.to_xml_string(test_suites, prettyprint=True)
+
+    def __generate_test_suites(self) -> list:
+        test_suites = []
+        for file_with_info in self.files_with_info:
+            test_suites.append(self.__create_test_suite_for(file_with_info))
+        return test_suites
 
     def __create_test_suite_for(self, file__with_info) -> TestSuite:
         test_case_list = self.__create_test_cases(file__with_info[0], file__with_info[1])
@@ -84,21 +94,18 @@ class JUnitReporter(Reporter):
         test_cases = []
 
         with file.open() as cmake_file:
-            self.current_parsing_line = 0
+            line_reader = LineReader(cmake_file)
 
-            for (violation_type, line_number) in violations:
-                self.__read_line_with_given_number(cmake_file, line_number)
-                test_case = TestCase(name=violation_type, line=line_number)
-                violation = "line: %4s %20s> %s" % (line_number, violation_type, self.current_violation_line)
-                test_case.add_failure_info(message=violation)
+            for (violation, line_number) in violations:
+                test_case = TestCase(name=violation, line=line_number)
+                test_case.add_failure_info(message=self.__generate_failure_info(line_number, violation, line_reader))
                 test_cases.append(test_case)
 
         return test_cases
 
-    def __read_line_with_given_number(self, file, line):
-        while self.current_parsing_line != line:
-            self.current_parsing_line = self.current_parsing_line + 1
-            self.current_violation_line = file.readline()
+    @staticmethod
+    def __generate_failure_info(line_number: int, violation_type: str, line_reader: LineReader) -> str:
+        return "line: %4s %20s> %s" % (line_number, violation_type, line_reader.get(line_number))
 
     @staticmethod
     def __generate_empty_test_suites() -> list:
